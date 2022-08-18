a84ac574f56617525007436400dbd698d5ef8664
teryokhinfit <terekhincw@gmail.com>
2022-08-19 01:12:11 +0400

    initial commit

---
+++ Main.java
@@ -0,0 +1,42 @@
+package JavaCore.LabmdHome;
+
+import java.util.function.*;
+
+public class Main {
+
+    public static void main(String[] args) {
+
+        Calculator calc = Calculator.instance.get();
+
+        int a = calc.plus.apply(1, 2);
+        int b = calc.minus.apply(1, 1);
+
+        if (a!= 0 || b != 0){
+            System.out.println("На ноль делить грешновато, исправим эту арифметическую ошибку:");
+            b = calc.abs.apply(b);
+        }
+
+        int c = calc.devide.apply(a, b); //арифметическая ошибка => на ноль делить нельзя, но код я исправил
+        calc.println.accept(c);
+    }
+
+}
+
+class Calculator {
+
+    static Supplier<Calculator> instance = Calculator::new;
+
+    BinaryOperator<Integer> plus = (x, y) -> x + y;
+    BinaryOperator<Integer> minus = (x, y) -> x - y;
+    BinaryOperator<Integer> multiply = (x, y) -> x * y;
+    BinaryOperator<Integer> devide = (x, y) -> x / y;
+
+    UnaryOperator<Integer> pow = x -> x * x;
+    UnaryOperator<Integer> abs = x -> x > 0 ? x : x + 1;
+
+    Predicate<Integer> isPositive = x -> x > 0;
+
+    Consumer<Integer> println = System.out::println;
+
+
+}
